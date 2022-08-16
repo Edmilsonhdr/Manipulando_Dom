5a42fa5290d6bee96c6f5e00f7bc7dc73b11613e
Edmilson Henrique Dias Rodrigues <92001139+Edmilsonhdr@users.noreply.github.com>
2022-08-16 10:32:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+var calcular = document.querySelector('#calcular');
+var resultado = document.querySelector('.resultado');
+
+calcular.addEventListener('click',()=>{resultado.innerHTML = "Fui clicado"});
